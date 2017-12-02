c3bf85952de9863c608bfbdc9f162748c168c22b
Matthew Traylor <mhtraylor@hotmail.com>
2017-12-02 17:49:29 -0600

    initial entity system

--- src/entity.js
+++ src/entity.js
@@ -1,5 +1,21 @@
-class Entity {
-    constructor(game) {
-        this.game = game
+import 'phaser'
+import { Some, None } from './functor'
+import { CharacterController } from './character-controller';
+
+export class Entity extends Phaser.Sprite {
+    constructor(game, pos, ach, name) {
+        super(game, pos[0], pos[1], name)
+        game.add.existing(this)
+        this.anchor.setTo(ach[0], ach[1])
+        this.components = []
+    }
+
+    addComponent(comp) {
+        this.components.push(comp(this))
+        return this
+    }
+
+    update() {
+        this.components.forEach(comp => comp.update())
     }
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import 'phaser';
 import pkg from '../package.json';
 import { CONFIG } from '../constants'
 
+import { Entity } from './entity'
+import { WASD_KEYS } from './input'
+import { PlayerController } from './character-controller'
+
 // This is the entry point of your game.
 
 const config = {
@@ -23,28 +27,51 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+let keys;
+let patrick;
+let pt;
+
 function preload() {
   this.game.load.image('dev_bkg', 'assets/img/dev_lvl_1536x480.png')
   this.game.load.spritesheet
     ( 'patrick'
     , 'assets/img/patrick-walk_64x64.png'
     , 64, 64, 4)
+
+    this.game.load.tilemap('map', 'assets/img/map.json', null, Phaser.Tilemap.TILED_JSON)
+    this.game.load.image('platform', 'assets/img/ground-sprite.png')
 }
 
 function create() {
   const { game } = this;
 
-  const dev_objs = [
-    game.add.sprite(0, 0, 'dev_bkg'),
-    game.add.sprite
-      ( game.world.centerX
-      , CONFIG.SCREEN.height - 110
-      , 'patrick' )
-  ]
+  keys = game.input.keyboard.createCursorKeys()
 
-  dev_objs[0].anchor.setTo(0, 0)
-  dev_objs[1].anchor.setTo(0.5, 0.5)
-  dev_objs[1].animations.add('walk')
-  dev_objs[1].animations.play('walk', 8, true)
+  const map = this.game.add.tilemap('map')
+  map.addTilesetImage('platform')
 
+  const layer_air = map.createLayer('air')
+
+  const layer_ground = map.createLayer('ground')
+
+  patrick = game.add.sprite(
+    game.world.centerX,
+    CONFIG.SCREEN.height - 110,
+    'patrick'
+  )
+
+  patrick.animations.add('walk')
+  patrick.animations.play('walk', 8, true)
+
+  pt = new Entity(game, [64, 64], [0.5, 0.5],'patrick')
+
+  pt.addComponent(
+    PlayerController(game, WASD_KEYS, {
+      speedX: 2
+    })
+  )
+}
+
+function update() {
+  pt.update()
 }
